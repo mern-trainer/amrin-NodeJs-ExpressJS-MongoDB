6b9b5962950fa6fee80aafa68b1370db37275ab3
MERN TRAINER <futuralabs.mern@gmail.com>
2024-12-04 12:54:41 +0530

    simple todo

--- app.js
+++ app.js
@@ -3,20 +3,42 @@ const { parse } = require("url")
 
 const server = createServer()
 
+let todoList = []
+
 server.on("request", (request, response) => {
 
-    const path = parse(request.url, true)
+    const { pathname, query } = parse(request.url, true)
+    const method = request.method
 
-    if (path.pathname === "/") {
+    if (pathname == "/api/todo/create" && method == "GET") {
+        const { title } = query
+        if (!title) {
+            response.writeHead(400, { "content-type": "application/json" });
+            return response.end(JSON.stringify({status: "Not OK", message: "Title is required!"}))
+        }
+        todoList.push(title)
         response.writeHead(201, { "content-type": "application/json" });
-        response.write(JSON.stringify({ age: 30 }));
-        return response.end()
+        return response.end(JSON.stringify({status: "OK", message: "Created!"}))
     }
 
-    if (path.pathname === "/api") {
-        response.writeHead(201, {"content-type": "application/json"})
-        response.write(JSON.stringify(path.query));
-        return response.end()
+    if (pathname == "/api/todo" && method == "GET") {
+        response.writeHead(200, { "content-type": "application/json" });
+        return response.end(JSON.stringify(todoList))
+    }
+
+    if (pathname == "/api/todo/update" && method == "GET") {
+        const { title, index } = query
+        const item = todoList[index]
+        todoList[index] = title
+        response.writeHead(201, { "content-type": "application/json" });
+        return response.end(JSON.stringify({message: `${item} updated to ${title}`}))
+    }
+
+    if (pathname == "/api/todo" && method == "DELETE") {
+        const { index } = query
+        todoList.splice(index, 1)
+        response.writeHead(200, { "content-type": "application/json" });
+        return response.end(JSON.stringify({status: "OK", message: "Task removed"}))
     }
 
 })
