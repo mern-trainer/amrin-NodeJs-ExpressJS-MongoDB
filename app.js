48576214d62905f2393c08905ee822b60b52dcbb
MERN TRAINER <futuralabs.mern@gmail.com>
2024-12-06 12:51:02 +0530

    added: todo list

--- app.js
+++ app.js
@@ -14,27 +14,80 @@
 // response, request
 
 const express = require("express")
+const { v4: taskId } = require("uuid")
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/:id?/:uid?", (request, response) => {
-    const { id, uid } = request.params
-    const query = request.query
-    if (!id) {
-        return response.status(400).send({ message: "Id is required!" });
+let todoList = []
+
+app.post("/todo", (request, response) => {
+    const { body } = request // title, description
+    body.id = taskId()
+    const dateTime = new Date().toISOString()
+    body.createdAt = dateTime
+    body.updatedAt = dateTime
+    const index = todoList.findIndex(todo => todo.title.toLowerCase() == body.title.toLowerCase())
+    if (index > -1) {
+        return response.status(409).send({
+            message: "Task already exist",
+            status: "Not OK"
+        })
     }
-    if (!uid) {
-        return response.status(400).send({ message: "Uid is required!" });
-    }
-    return response.status(200).send({...request.params, ...query});
+    todoList.push(body)
+    return response.status(201).send({
+        message: "New Task Created!",
+        status: "OK",
+        body
+    })
 })
 
-app.post("/api", (request, response) => {
-    const body = request.body
-    console.log(body)
-    return response.status(200).send(body)
+app.get("/todo", (request, response) => {
+    return response.status(200).send({
+        message: "All List",
+        status: "OK",
+        todoList
+    }) 
+})
+
+app.delete("/todo/:id?", (request, response) => {
+    const { id } = request.params
+    if (!id) {
+        return response.status(400).send({
+            message: "Id is required",
+            status: "Not OK"
+        })
+    }
+    const todo = todoList.find(item => item.id == id)
+    todoList = todoList.filter(todo => todo.id !== id)
+    return response.status(200).send({
+        message: "Task Removed",
+        status: "OK",
+        deleted: todo
+    }) 
+})
+
+app.patch("/todo/:id?", (request, response) => { 
+    const { id } = request.params
+    const { body } = request
+    if (!id) {
+        return response.status(400).send({
+            message: "Id is required",
+            status: "Not OK"
+        })
+    }
+    todoList = todoList.map(todo => {
+        if (todo.id == id) {
+            return { ...todo, ...body }
+        }
+        return todo
+    })
+    return response.status(200).send({
+        message: "Task Updated",
+        status: "OK",
+        updated: todoList.find(todo => todo.id == id)
+    })
 })
 
 app.listen(8080, (err) => {
